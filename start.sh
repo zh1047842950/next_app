a314dd9cb2fa8cbeafff12ff3f71645c540f6348
zhonghongyan@coocaa.com <zhonghongyan@coocaa.com>
2021-03-23 15:09:07 +0800

    next_env:v1 -> next_env:v2

--- start.sh
+++
@@ -1,9 +0,0 @@
-  #!bin/bash
-  now=`date '+%Y-%m-%d %H:%M:%S'`
-  echo "Hello next_app !"
-  cd /workspace/next_app
-  source /etc/profile
-  npm -V
-  npm run start
-  echo "Update react_app success!"$now
-
