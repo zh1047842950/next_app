bd4d5c42fecb46c6a2e1f2621e1130b98912c24d
zhonghongyan <zhonghongyan@coocaa.com>
2026-03-10 11:28:21 +0800

    webhook 测试

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ class Index extends React.Component<P, S> {
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
-            <div className={'home flex'}>
+            <div className={'home flex 0'}>
                 <Navs />
                 <h1>Home Page</h1>
             </div>
